3889aaa2f5c7783280ea133cbe4f02bd44ededb7
Dotennin <wenhua4438@gmail.com>
2022-01-21 00:44:42 +0900

    feat: add action filter

--- src/utils.tsx
+++ src/utils.tsx
@@ -76,3 +76,14 @@ export function getText(
     return type;
   }
 }
+
+export const debounce = <T extends (...args: unknown[]) => unknown>(
+  callback: T,
+  delay = 250,
+): ((...args: Parameters<T>) => void) => {
+  let timeoutId: NodeJS.Timeout
+  return (...args) => {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => callback(...args), delay)
+  }
+}
